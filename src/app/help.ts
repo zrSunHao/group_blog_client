8958b73421c3bd00c33e925d97b15d7cdc98c882
sunhao <2080680175@qq.com>
2022-07-17 11:42:52 +0800

    editor code and list!

--- src/app/help.ts
+++ src/app/help.ts
@@ -8,9 +8,9 @@ export const DOCUMENT_DATA: DocumentNode[] = [
                     { type: DocumentNodeType.p, content: '一是${就业主体人群|color:red;font-weight:bold;}就业${总体稳定|background:green}。25-59岁就业主体人群失业率4月份虽有所升高，但是升幅明显低于总体失业率水平。6月份，25-59岁就业主体人群失业率为4.5%，比上月回落0.6个百分点，接近2021年平均水平。', url: '', remark: '', data: {}, order: 1, children: [] },
                     { type: DocumentNodeType.p, content: '二是${农民工群体|color:red}就业形势好转。农民工群体多从事劳动密集型制造业和服务业行业，以个体户和灵活就业的居多，就业稳定性相对较差。受疫情冲击影响，4月份外来农业户籍人口失业率升至6.6%，随着疫情逐步得到控制，助企稳岗措施发力，农民工群体的就业明显改善。5月份外来农业户籍人口失业率降至6.2%，6月份降至5.3%，低于整体失业率水平。', url: '', remark: '', data: {}, order: 2, children: [] },
                     { type: DocumentNodeType.p, content: '三是${青年人|color:red}就业压力仍然较大。青年群体初次进入劳动力市场，普遍面临摩擦性失业困境，同时受疫情影响，企业吸纳就业能力下降，年轻人求职渠道在疫情条件下也受到了阻碍。加之今年高校毕业生总量又创历史新高，加剧了青年人就业压力。6月份，16-24岁城镇青年失业率为19.3%，处于今年以来较高水平。', url: '', remark: '', data: {}, order: 3, children: [] },
-                    { type: DocumentNodeType.img, content: '今日入伏 一年中最热时段来了', url: 'assets/imgs/bg_4.png', remark: '', data: { height: '15rem', position: 'left' }, order: 4, children: [] },
-                    { type: DocumentNodeType.img, content: '', url: '', remark: '', data: { height: '15rem', position: 'left' }, order: 4, children: [] },
-                    { type: DocumentNodeType.img, content: '', url: '', remark: '', data: { height: '15rem', position: 'left' }, order: 4, children: [] },
+                    { type: DocumentNodeType.img, content: '今日入伏 一年中最热时段来了', url: 'assets/imgs/bg_4.png', remark: '', data: { height: '20rem', position: 'center' }, order: 4, children: [] },
+                    { type: DocumentNodeType.img, content: '', url: '', remark: '', data: { height: '15rem', position: 'center' }, order: 4, children: [] },
+                    { type: DocumentNodeType.img, content: '', url: '', remark: '', data: { height: '15rem', position: 'center' }, order: 4, children: [] },
                 ]
             },
             { type: DocumentNodeType.h3, content: '阆中起价1.8亿拍卖食堂30年经营权', url: '', remark: '', data: {}, order: 2, children: [] },
@@ -21,6 +21,9 @@ export const DOCUMENT_DATA: DocumentNode[] = [
             },
         ]
     },
-    { type: DocumentNodeType.h2, content: '上海二季度GDP同比下降13.7%', url: '', remark: '', data: {}, order: 2, children: [] },
+    { type: DocumentNodeType.h2, content: '上海二季度GDP同比下降13.7%', url: '', remark: '', data: {}, order: 2, children: [
+        { type: DocumentNodeType.list, content: '在 XAML 和 C# 中从 Visual Studio 中的单个共享代码库编写跨平台应用。\n跨平台共享 UI 布局和设计。\n跨平台共享代码、测试和业务逻辑。', url: '', remark: '', data: {}, order: 2, children: [] },
+        { type: DocumentNodeType.code, content: '[XamlCompilation (XamlCompilationOptions.Compile)]\npublic partial class MyPage : ContentPage\n{\n     ...\n}', url: '', remark: '', data: {}, order: 2, children: [] },
+    ] },
     { type: DocumentNodeType.h2, content: '活虾从菜场拎到家被“热熟”', url: '', remark: '', data: {}, order: 3, children: [] },
 ];
--- src/app/sun-editor/editor-list/editor-list.component.ts
+++ src/app/sun-editor/editor-list/editor-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { EditorService, DocumentNode } from '../editor.service';
 
 @Component({
@@ -9,10 +9,48 @@ import { EditorService, DocumentNode } from '../editor.service';
 export class EditorListComponent implements OnInit {
 
   @Input() node: DocumentNode = new DocumentNode();
+  @ViewChild('input', { static: false })
+  input!: ElementRef;
+  edit: boolean = false;
+  focus: boolean = false;
+  list: string[] = [];
 
   constructor(public service: EditorService) { }
 
   ngOnInit() {
+    this.onStatusChange();
   }
 
+  onInputFocus() {
+    this.focus = true;
+    this.onStatusChange();
+    setTimeout(() => {
+      if (this.input) {
+        this.input.nativeElement.focus();
+      }
+    }, 150);
+  }
+
+  onInputBlur() {
+    this.focus = false;
+    this.onStatusChange();
+  }
+
+  private onStatusChange() {
+    if (!this.node.content) this.edit = true;
+    else {
+      if (this.focus) this.edit = true;
+      else {
+        this.edit = false;
+        this.onParseContent();
+      }
+    }
+  }
+
+  private onParseContent() {
+    this.list = [];
+    if (!this.node.content) return;
+    let bs = this.node.content.split('\n');
+    this.list = bs.filter(x => x);
+  }
 }
